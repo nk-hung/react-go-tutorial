19cfe2ba792d1d17665707fe51f63eef6088be91
hungnk <nguyenkyhunght@gmail.com>
2024-08-29 23:00:28 +0700

    feat: using air reload when file change

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Todo struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
 
 func main() {
-	fmt.Print("Hello world")
+	fmt.Println("hello world. I'm here. Wellcome to my hold world")
+
+	app := fiber.New()
+
+	todos := []Todo{}
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{
+			"msg": "Hello Babe",
+		})
+	})
+
+	app.Post("/todo", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Todo body is required!"})
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Status(200).JSON(todo)
+	})
+
+	log.Fatal(app.Listen(":8000"))
 }
